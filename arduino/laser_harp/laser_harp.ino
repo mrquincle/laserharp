7a446131e1c51660bc7b976139abfdc6b3cfc620
mrquincle <anne@crownstone.rocks>
2022-03-11 17:48:48 +0100

    Laser and motor in same sketch

--- arduino/laser_harp/laser_harp.ino
+++ arduino/laser_harp/laser_harp.ino
@@ -1,42 +1,60 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 
+const int motorPort = 2;
+const int motorSpeed = 55;
+const int motorSwing = 20;
+const int laserPin = 7;
+const int beamCount = 4;
+const long baudRate = 115200;
+const int lightSensorPin = A0;
+const int interBeamDelay = 100;
+
+int loopCounter = 0;
+
+const bool motorEnabled = true;
+
 // Create the motor shield object with the default I2C address
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-// Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
-// Connect a stepper motor with 200 steps per revolution (1.8 degree)
-// to motor port #2 (M3 and M4)
-Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
-
-const int laserPin = 1;
+// Connect a stepper motor with 200 steps per revolution (1.8 degree) to motor port #2 (M3 and M4)
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, motorPort);
 
 void setup() {
-  for (int i = 0; i < 7; ++i) {
-	  pinMode(i, OUTPUT);
-    digitalWrite(i, LOW);
-  }
-    
+	// First set the first 8 pins as outputs and drive them low
+	for (int i = 0; i < 7; ++i) {
+		pinMode(i, OUTPUT);
+		digitalWrite(i, LOW);
+	}
 
-  AFMS.begin();
-  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
-  
-  myMotor->setSpeed(10);  // 10 rpm   
+	AFMS.begin();
+	myMotor->setSpeed(motorSpeed);
 
-  Serial.begin(115200);
-  Serial.println("Start");
+	Serial.begin(baudRate);
+	Serial.println("Start");
 }
 
-int counter = 0;
-
 void loop() {
-  Serial.print("Step ");
-  Serial.println(counter++);
-  myMotor->step(10, FORWARD, MICROSTEP); 
-  myMotor->step(10, BACKWARD, MICROSTEP);
-  delay(1000);
-  digitalWrite(4, HIGH);
-  delay(1000);
-  digitalWrite(4, LOW);
+	Serial.print("Step ");
+	Serial.println(loopCounter++);
+	
+	for (int i = 0; i < beamCount; ++i) {
+		if (motorEnabled) {
+			myMotor->step(motorSwing/beamCount, FORWARD, DOUBLE);
+		}
+		digitalWrite(laserPin, HIGH);
+		int lightIntensity = analogRead(A0);
+		Serial.println(lightIntensity);
+		delay(interBeamDelay);
+		digitalWrite(laserPin, LOW);
+	}
+	for (int i = 0; i < beamCount; ++i) {
+		if (motorEnabled) {
+			myMotor->step(motorSwing/beamCount, BACKWARD, DOUBLE);
+		}
+		digitalWrite(laserPin, HIGH);
+		delay(interBeamDelay);
+		digitalWrite(laserPin, LOW);
+	}
+	delay(1000);
 }
